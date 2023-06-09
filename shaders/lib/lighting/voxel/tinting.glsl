e1d8d902699210b63588fd28780b03fc39a8d7cc
NULL511 <null511@outlook.com>
2023-06-08 20:34:00 -0400

    less leaf tinting

--- shaders/lib/lighting/voxel/tinting.glsl
+++ shaders/lib/lighting/voxel/tinting.glsl
@@ -6,7 +6,7 @@ vec3 GetLightGlassTint(const in uint blockId) {
             stepTint = vec3(0.984, 0.733, 0.251);
             break;
         case BLOCK_LEAVES:
-            stepTint = vec3(0.562, 0.833, 0.475);
+            stepTint = vec3(0.620, 0.778, 0.569);
             break;
         case BLOCK_ROOTS:
             stepTint = vec3(0.650, 0.700, 0.650);
