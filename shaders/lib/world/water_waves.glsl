9d54a20099d3d850dcfa00d7657d2f2b20ccfa64
NULL511 <null511@outlook.com>
2023-08-10 20:01:01 -0400

    improve medium, large waves

--- shaders/lib/world/water_waves.glsl
+++ shaders/lib/world/water_waves.glsl
@@ -7,14 +7,14 @@ const float WATER_SPEED_MULT = 1.08;
 const float WATER_ITER_INC = PI * (3.0 - sqrt(5.0));
 
 #if   WORLD_WATER_WAVES == 3
-    #define WATER_ITERATIONS_FRAGMENT 38
+    #define WATER_ITERATIONS_FRAGMENT 34
     const float WATER_XZ_SCALE = 0.8;
-    const float WATER_DRAG_MULT = 0.4;
-    const float WATER_DRAG_INC = 0.6;
+    const float WATER_DRAG_MULT = 0.6;
+    const float WATER_DRAG_INC = 0.06;
     const float WATER_WAVE_HEIGHT = 0.5;
-    const float WATER_SPEED = 1.8;
-    const float WATER_WEIGHT = 0.1;
-    const float WATER_NORMAL_STRENGTH = 0.5;
+    const float WATER_SPEED = 1.5;
+    const float WATER_WEIGHT = 0.4;
+    const float WATER_NORMAL_STRENGTH = 0.08;
 #elif WORLD_WATER_WAVES == 2
     #define WATER_ITERATIONS_FRAGMENT 26
     const float WATER_XZ_SCALE = 1.4;
@@ -23,7 +23,7 @@ const float WATER_ITER_INC = PI * (3.0 - sqrt(5.0));
     const float WATER_WAVE_HEIGHT = 0.3;
     const float WATER_SPEED = 2.0;
     const float WATER_WEIGHT = 0.6;
-    const float WATER_NORMAL_STRENGTH = 0.25;
+    const float WATER_NORMAL_STRENGTH = 0.125;
 #elif WORLD_WATER_WAVES == 1
     #define WATER_ITERATIONS_FRAGMENT 18
     const float WATER_XZ_SCALE = 3.2;
