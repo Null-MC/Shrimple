e98825ca86ccd37f81f65a28edb15973290c8b70
NULL511 <null511@outlook.com>
2023-08-13 22:50:20 -0400

    water amd const fix

--- shaders/lib/world/water_waves.glsl
+++ shaders/lib/world/water_waves.glsl
@@ -11,14 +11,14 @@ const float WATER_NORMAL_STRENGTH = 0.25;
 #if   WORLD_WATER_WAVES == 3
     #define WATER_ITERATIONS_FRAGMENT 42
     const float WATER_XZ_SCALE = 0.6;
-    const float WATER_DRAG_MULT = mix(0.2, 0.6, rainStrength);
+    float WATER_DRAG_MULT = mix(0.2, 0.6, rainStrength);
     const float WATER_DRAG_INC = 0.06;
     const float WATER_WAVE_HEIGHT = 0.8;
     const float WATER_SPEED = 1.5;
 #elif WORLD_WATER_WAVES == 2
     #define WATER_ITERATIONS_FRAGMENT 32
     const float WATER_XZ_SCALE = 1.4;
-    const float WATER_DRAG_MULT = mix(0.2, 0.4, rainStrength);
+    float WATER_DRAG_MULT = mix(0.2, 0.4, rainStrength);
     const float WATER_DRAG_INC = 0.06;
     const float WATER_WAVE_HEIGHT = 0.4;
     const float WATER_SPEED = 2.0;
