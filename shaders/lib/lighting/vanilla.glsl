0e0fea26ceec9470a65b2baaac7f42acd326e2b5
NULL511 <null511@outlook.com>
2023-12-22 23:49:21 -0500

    more directional ambient sky light

--- shaders/lib/lighting/vanilla.glsl
+++ shaders/lib/lighting/vanilla.glsl
@@ -12,12 +12,13 @@
 void GetVanillaLighting(out vec3 diffuse, const in vec2 lmcoord, const in vec3 localPos, const in vec3 localNormal, const in vec3 texNormal, in vec3 shadowColor, in float sss) {
     vec2 lmFinal = lmcoord;
 
-    #if defined WORLD_SKY_ENABLED && !defined RENDER_SHADOWS_ENABLED
-        float skyNoLm = max(dot(texNormal, localSkyLightDirection), 0.0);
+    // #if defined WORLD_SKY_ENABLED //&& !defined RENDER_SHADOWS_ENABLED
+    //     float skyNoLm = max(dot(texNormal, localSkyLightDirection), 0.0);
+    //     //float skyNoLm = dot(texNormal, localSkyLightDirection) * 0.5 + 0.5;
 
-        float sunAngleRange = 0.5 * localSkyLightDirection.y;
-        lmFinal.y *= skyNoLm * sunAngleRange + (1.0 - sunAngleRange);
-    #endif
+    //     float sunAngleRange = (1.0 - DynamicLightAmbientF) * localSkyLightDirection.y;
+    //     lmFinal.y *= skyNoLm * sunAngleRange + (1.0 - sunAngleRange);
+    // #endif
 
     lmFinal = LightMapTex(lmFinal);
 
@@ -38,6 +39,7 @@ void GetVanillaLighting(out vec3 diffuse, const in vec2 lmcoord, const in vec3 l
         float horizonF = smoothstep(0.0, 0.12, abs(localSkyLightDirection.y));
 
         float ambientF = DynamicLightAmbientF;
+        ambientF *= dot(texNormal, localSkyLightDirection) * 0.25 + 0.75;
         ambientF = 1.0 - (1.0 - ambientF) * horizonF;
 
         vec3 ambientLight = vec3(ambientF);
