b1bf6476cb230bcb61f4d76d4cf5c219202a789b
NULL511 <null511@outlook.com>
2024-01-11 01:41:15 -0500

    adjust rain vl

--- shaders/lib/world/atmosphere.glsl
+++ shaders/lib/world/atmosphere.glsl
@@ -2,9 +2,9 @@ const float AirDensityF = SKY_DENSITY * 0.01;
 const float phaseAir = phaseIso;
 
 #ifdef WORLD_SKY_ENABLED
-	const float AirDensityRainF = 2.0 * AirDensityF; //0.05;
+	const float AirDensityRainF = AirDensityF; //0.05;
 	const float AirScatterRainF = 0.48;
-	const float AirExtinctRainF = 0.24;
+	const float AirExtinctRainF = 0.48;
 
 	float AirAmbientF = 0.02;//mix(0.02, 0.0, skyRainStrength);
 	float AirScatterF = 0.32;//mix(1.00, 0.028, skyRainStrength);
