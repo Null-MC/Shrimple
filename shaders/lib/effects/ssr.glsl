ab6579eaa8cce84b84f3a0f8d54013af7c774e70
NULL511 <null511@outlook.com>
2024-02-24 22:55:24 -0500

    cleanup

--- shaders/lib/effects/ssr.glsl
+++ shaders/lib/effects/ssr.glsl
@@ -52,51 +52,44 @@ vec4 GetReflectionPosition(const in sampler2D depthtex, const in vec3 clipPos, c
     #endif
 
     float startDepthLinear = linearizeDepth(clipPos.z, near, _far);
-    //ivec2 iuv_start = ivec2(clipPos.xy * viewSize);
 
-    #if defined MATERIAL_REFLECT_HIZ && SSR_LOD_MAX > 0
-        vec2 origin = clipPos.xy * viewSize + screenRay.xy * dither;
-        vec3 direction = screenRay;
+    // #if defined MATERIAL_REFLECT_HIZ && SSR_LOD_MAX > 0
+    //     vec2 origin = clipPos.xy * viewSize + screenRay.xy * dither;
+    //     vec3 direction = screenRay;
 
-        vec2 stepDir = sign(direction.xy);
-        vec2 stepSizes = rcp(abs(direction.xy));// * viewSize;
-        vec2 nextDist = (stepDir * 0.5 + 0.5 - fract(origin)) / direction.xy;
-    #endif
+    //     vec2 stepDir = sign(direction.xy);
+    //     vec2 stepSizes = rcp(abs(direction.xy));// * viewSize;
+    //     vec2 nextDist = (stepDir * 0.5 + 0.5 - fract(origin)) / direction.xy;
+    // #endif
 
     vec2 screenRayAbs = abs(screenRay.xy);
     vec3 rayX = screenRay * (pixelSize.x / screenRayAbs.x);
     vec3 rayY = screenRay * (pixelSize.y / screenRayAbs.y);
     screenRay = mix(rayX, rayY, screenRayAbs.y);
 
-    // if (screenRayAbs.y > screenRayAbs.x / aspectRatio)
-    //     screenRay *= pixelSize.y / screenRayAbs.y;
-    // else
-    //     screenRay *= pixelSize.x / screenRayAbs.x;
-
     int level = 0;
-    #ifndef MATERIAL_REFLECT_HIZ
-        //screenRay *= 8.0;
-        level = 0;//clamp(int(log2(maxOf(viewSize) / SSR_MAXSTEPS + 1.0)), 0, 5);
-    #endif
+    // #ifndef MATERIAL_REFLECT_HIZ
+    //     //screenRay *= 8.0;
+    //     level = 0;//clamp(int(log2(maxOf(viewSize) / SSR_MAXSTEPS + 1.0)), 0, 5);
+    // #endif
 
     vec3 lastTracePos = clipPos + screenRay * (1.0 + dither);
     vec3 lastVisPos = lastTracePos;
 
 
-    #if defined MATERIAL_REFLECT_HIZ && SSR_LOD_MAX > 0
-        float closestDist = minOf(nextDist);
-        vec2 currPos = origin + direction.xy * closestDist;
-        vec2 lastPos = currPos;
+    // #if defined MATERIAL_REFLECT_HIZ && SSR_LOD_MAX > 0
+    //     float closestDist = minOf(nextDist);
+    //     vec2 currPos = origin + direction.xy * closestDist;
+    //     vec2 lastPos = currPos;
 
-        bvec2 stepAxis = lessThanEqual(nextDist, vec2(closestDist));
+    //     bvec2 stepAxis = lessThanEqual(nextDist, vec2(closestDist));
 
-        nextDist -= closestDist;
-        nextDist += stepSizes * vec2(stepAxis);
-    #endif
+    //     nextDist -= closestDist;
+    //     nextDist += stepSizes * vec2(stepAxis);
+    // #endif
 
 
-    // vec3 clipMin = vec3(pixelSize, 0.0);
-    // vec3 clipMax = vec3(1.0) - clipMin;
+    const vec3 clipMin = vec3(0.0);
 
     int i;
     float alpha = 0.0;
@@ -107,26 +100,18 @@ vec4 GetReflectionPosition(const in sampler2D depthtex, const in vec3 clipPos, c
         tracePos = screenRay*stepScale + lastTracePos;
 
 
-        #if defined MATERIAL_REFLECT_HIZ && SSR_LOD_MAX > 0
-            closestDist = minOf(nextDist);
-            vec2 ddaStep = direction.xy * closestDist * stepScale;
+        // #if defined MATERIAL_REFLECT_HIZ && SSR_LOD_MAX > 0
+        //     closestDist = minOf(nextDist);
+        //     vec2 ddaStep = direction.xy * closestDist * stepScale;
 
-            //float currLen2 = length2(currPos - origin);
-            //if (currLen2 > traceRayLen2) currPos = endPos;
-            
-            //vec3 voxelPos = floor(0.5 * (currPos + rayStart));
+        //     stepAxis = lessThanEqual(nextDist, vec2(closestDist));
 
-            //if (ivec3(0.5 * (currPos + rayStart)) == ivec3(origin)) continue;
-
-            stepAxis = lessThanEqual(nextDist, vec2(closestDist));
-
-            nextDist -= closestDist;
-            nextDist += stepSizes * vec2(stepAxis);
-        #endif
+        //     nextDist -= closestDist;
+        //     nextDist += stepSizes * vec2(stepAxis);
+        // #endif
 
 
-        vec3 clipMin = vec3(pixelSize, 0.0) * stepScale;
-        vec3 clipMax = vec3(1.0) - clipMin;
+        vec3 clipMax = vec3(1.0) - vec3(pixelSize * stepScale, 0.0);
 
         vec3 t = clamp(tracePos, clipMin, clipMax);
         if (t != tracePos) {
@@ -135,27 +120,19 @@ vec4 GetReflectionPosition(const in sampler2D depthtex, const in vec3 clipPos, c
                 continue;
             }
 
-            lastVisPos = t;//vec3(t.xy, saturate(tracePos.z));
+            lastVisPos = t;
             if (tracePos.z >= 1.0 && t.xy == tracePos.xy) alpha = 1.0;
             break;
         }
 
-        //float depthBias = -0.01 * (1.0 - clipPos.z);
-        //texDepth = SampleDepthTiles(depthtex, tracePos.xy, level);
         #if defined MATERIAL_REFLECT_HIZ && SSR_LOD_MAX > 0
             //texDepth = SampleDepthTiles(depthtex, currPos / viewSize, level);
             float sampleDepthL = SampleDepthTiles(depthtex, 0.5*(lastPos + currPos) / viewSize, level);
         #else
-            //texDepth = SampleDepthTiles(depthtex, tracePos.xy, 0);
-            //texDepth = texelFetch(depthtex, ivec2(tracePos.xy * viewSize), 0).r;
-            //texDepth = textureLod(depthtex, tracePos.xy, 4).r;
-
             float sampleDepthL = SampleDepthTiles(depthtex, tracePos.xy, level);
         #endif
 
-        //float minTraceDepth = min(tracePos.z, lastTracePos.z);
         float traceDepthL = linearizeDepth(tracePos.z, near, _far);
-        //float sampleDepthL = linearizeDepthFast(texDepth, near, far);
 
         float bias = 0.002;//0.1 * sampleDepthL;
         bool isCloserThanStartAndMovingAway = false;//startDepthLinear > sampleDepthL + bias && screenRay.z > 0.0;
@@ -163,46 +140,28 @@ vec4 GetReflectionPosition(const in sampler2D depthtex, const in vec3 clipPos, c
         //bool isTraceNearerThanStart = traceDepthL < sampleDepthL + 0.1;
         bool isTooThickAndMovingNearer = false;//traceDepthL > sampleDepthL + 1.0 && screenRay.z < 0.0;
 
-        // if (isTraceNearerThanSample && !isCloserThanStartAndMovingAway) {
-        //     lastVisPos = tracePos;
-        //     alpha = 1.0;
-        // }
-
         if (isTraceNearerThanSample || isCloserThanStartAndMovingAway || isTooThickAndMovingNearer) {
             lastTracePos = tracePos;
 
-            #if defined MATERIAL_REFLECT_HIZ && SSR_LOD_MAX > 0
-                lastPos = currPos;
-                currPos += ddaStep;
-            #endif
+            // #if defined MATERIAL_REFLECT_HIZ && SSR_LOD_MAX > 0
+            //     lastPos = currPos;
+            //     currPos += ddaStep;
+            // #endif
 
-            if (level < SSR_LOD_MAX) {
-                //vec2 halfPos = floor(currPos + 0.5) * 0.5;
-                //if (all(greaterThan(abs(fract(halfPos) - 0.5), vec2(0.49)))) level++;
-
-                level++;
-            }
+            if (level < SSR_LOD_MAX) level++;
 
             continue;
         }
 
-        //#if defined MATERIAL_REFLECT_HIZ && SSR_LOD_MAX > 0
-            if (level > SSR_LodMin && i < SSR_MAXSTEPS - (level + 1)) {
-               level--;
-               continue;
-            }
-        //#endif
+        if (level > SSR_LodMin && i < SSR_MAXSTEPS - (level + 1)) {
+           level--;
+           continue;
+        }
 
-        //lastTracePos = tracePos;
         lastVisPos = tracePos;
         alpha = 1.0;
         break;
     }
-
-    // if (i == SSR_MAXSTEPS) {
-    //     lastVisPos = tracePos;
-    //     alpha = 1.0;
-    // }
 
     #ifdef SSR_DEBUG
         alpha *= level + 0.1;
