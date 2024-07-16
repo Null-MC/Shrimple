0f2427f9227cc357bb9ecc143464f8f0b4325ca2
NULL511 <null511@outlook.com>
2024-07-16 16:49:50 -0400

    misc fixes

--- shaders/lib/lighting/vanilla.glsl
+++ shaders/lib/lighting/vanilla.glsl
@@ -1,10 +1,12 @@
 void GetVanillaLighting(out vec3 diffuse, in vec2 lmcoord, const in vec3 shadowColor, const in float occlusion) {//, const in vec3 localPos, const in vec3 localNormal, const in vec3 texNormal, in vec3 shadowColor, in float sss) {
     #if LIGHTING_MODE == LIGHTING_MODE_VANILLA
-        float shadowF = luminance(shadowColor); // WARN: fix scaling!
-        lmcoord.y *= shadowF;
+        #ifdef RENDER_SHADOWS_ENABLED
+            float shadowF = luminance(shadowColor);
+            lmcoord.y *= shadowF; // WARN: fix scaling!
+        #endif
 
         vec3 lightmapFinal = textureLod(TEX_LIGHTMAP, LightMapTex(lmcoord), 0).rgb;
-        diffuse = RGBToLinear(lightmapFinal);// * blackbody(LIGHTING_TEMP);
+        diffuse = RGBToLinear(lightmapFinal);
     #else
         vec3 lightmapBlock = _pow3(lmcoord.x) * blackbody(LIGHTING_TEMP);
         diffuse = lightmapBlock * Lighting_Brightness;
@@ -14,8 +16,6 @@ void GetVanillaLighting(out vec3 diffuse, in vec2 lmcoord, const in vec3 shadowC
 }
 
 vec3 GetFinalLighting(const in vec3 albedo, in vec3 diffuse, in vec3 specular, const in float metal_f0, const in float roughL, const in float emission, const in float occlusion) {
-    // ApplyMetalDarkening(diffuse, specular, albedo, metal_f0, roughL);
-
     #if DEBUG_VIEW == DEBUG_VIEW_WHITEWORLD
         vec3 final = vec3(WHITEWORLD_VALUE);
     #else
--- shaders/lib/shadows/distorted/render_sss.glsl
+++ shaders/lib/shadows/distorted/render_sss.glsl
@@ -1,43 +1,3 @@
-// const float minShadowPixelRadius = Shadow_MinPcfSize * shadowPixelSize;
-
-
-// float GetSssDither() {
-//     #ifdef RENDER_FRAG
-//         #ifdef EFFECT_TAA_ENABLED
-//             return InterleavedGradientNoiseTime();
-//         #else
-//             return InterleavedGradientNoise();
-//         #endif
-//     #else
-//         return 0.0;
-//     #endif
-// }
-
-// float SampleDepth(const in vec2 shadowPos) {
-//     #ifdef RENDER_TRANSLUCENT
-//         return texture(shadowtex0, shadowPos).r;
-//     #else
-//         return texture(shadowtex1, shadowPos).r;
-//     #endif
-// }
-
-// returns: [0] when depth occluded, [1] otherwise
-// float CompareDepth(in vec3 shadowPos, const in vec2 offset, const in float bias) {
-//     shadowPos = distort(shadowPos + vec3(offset, -bias));
-//     shadowPos = shadowPos * 0.5 + 0.5;
-
-//     #if defined SHADOW_ENABLE_HWCOMP && defined IRIS_FEATURE_SEPARATE_HARDWARE_SAMPLERS
-//         #ifdef RENDER_TRANSLUCENT
-//             return texture(shadowtex0HW, shadowPos).r;
-//         #else
-//             return texture(shadowtex1HW, shadowPos).r;
-//         #endif
-//     #else
-//         float texDepth = SampleDepth(shadowPos.xy);
-//         return step(shadowPos.z, texDepth);
-//     #endif
-// }
-
 float GetSss_PCF(const in vec3 shadowPos, const in vec2 pixelRadius, const in float offsetBias, const in float sssBias) {
     float dither = GetShadowDither();
 
@@ -65,23 +25,6 @@ float GetSss_PCF(const in vec3 shadowPos, const in vec2 pixelRadius, const in fl
     return 1.0 - shadow * rcp(SHADOW_SSS_SAMPLES);
 }
 
-// vec2 GetSssPixelRadius(const in vec3 shadowPos, const in float blockRadius) {
-//     // #ifndef IRIS_FEATURE_SSBO
-//     //     mat4 shadowProjectionEx = shadowProjection;//BuildShadowProjectionMatrix();
-//     //     shadowProjectionEx[2][2] = -2.0 / (3.0 * far);
-//     //     shadowProjectionEx[3][2] = 0.0;
-//     // #endif
-
-//     vec2 shadowProjectionSize = 2.0 / vec2(shadowProjectionEx[0].x, shadowProjectionEx[1].y);
-
-//     //float distortFactor = getDistortFactor(shadowPos.xy * 2.0 - 1.0);
-//     //float maxRes = shadowMapSize / Shadow_DistortF;
-
-//     vec2 pixelPerBlockScale = shadowMapSize / shadowProjectionSize;
-//     return 2.0 * blockRadius * pixelPerBlockScale * shadowPixelSize;// * (1.0 - distortFactor);
-// }
-
-// PCF
 float GetSssFactor(const in vec3 shadowPos, const in float offsetBias, const in float sss) {
     float zRange = GetShadowRange();
     float sssBias = sss * MATERIAL_SSS_MAXDIST / zRange;
