33cb4559893af9411ef1507c1788acdee60cfa36
NULL511 <null511@outlook.com>
2025-05-21 14:32:11 -0400

    revert planet curve function

--- shaders/lib/world/curvature.glsl
+++ shaders/lib/world/curvature.glsl
@@ -1,11 +1,13 @@
-// vec3 GetWorldCurvedPosition(in vec3 position) {
-//     // horizon-stretching fix by cr0420
-//     position.xz *= 1.0 + length2(position.xz) / _pow2(WORLD_CURVE_RADIUS) * 0.25;
+ vec3 GetWorldCurvedPosition(in vec3 position) {
+     // horizon-stretching fix by cr0420
+     position.xz *= 1.0 + length2(position.xz) / _pow2(WORLD_CURVE_RADIUS) * 0.25;
 
-//     position.y += WORLD_CURVE_RADIUS;
-//     position = normalize(position) * position.y;
-//     position.y -= WORLD_CURVE_RADIUS;
-// }
+     position.y += WORLD_CURVE_RADIUS;
+     position = normalize(position) * position.y;
+     position.y -= WORLD_CURVE_RADIUS;
+
+     return position;
+ }
 
 float GetWorldAltitude(in vec3 localPos) {
     localPos.y += World_CurveRadius + cameraPosition.y;
@@ -13,14 +15,14 @@ float GetWorldAltitude(in vec3 localPos) {
 }
 
 // by cr0420
-vec3 GetWorldCurvedPosition(in vec3 localPos) {
-    const float WorldCurveRadiusInv = rcp(World_CurveRadius);
-
-    vec3 angle = vec3(localPos.x, length(localPos.xz), localPos.z) * WorldCurveRadiusInv;
-    float planetHeight = localPos.y + World_CurveRadius;
-    
-    localPos.y  = cos(angle.y) * planetHeight - World_CurveRadius;
-    localPos.xz = sin(angle.xz) * planetHeight;
-
-    return localPos;
-}
+//vec3 GetWorldCurvedPosition(in vec3 localPos) {
+//    const float WorldCurveRadiusInv = rcp(World_CurveRadius);
+//
+//    vec3 angle = vec3(localPos.x, length(localPos.xz), localPos.z) * WorldCurveRadiusInv;
+//    float planetHeight = localPos.y + World_CurveRadius;
+//
+//    localPos.y  = cos(angle.y) * planetHeight - World_CurveRadius;
+//    localPos.xz = sin(angle.xz) * planetHeight;
+//
+//    return localPos;
+//}
