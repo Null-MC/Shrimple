fd55f3d929770b55f638082f46b192c5bc498888
NULL511 <null511@outlook.com>
2025-09-22 12:30:45 -0400

    misc fixes

--- shaders/program/voxy_opaque.glsl
+++ shaders/program/voxy_opaque.glsl
@@ -34,58 +34,98 @@
     #endif
 #endif
 
-float computeBlockEmission(uint blockId) {
-//    uint lightType = GetSceneLightType(blockId);
-//
-//    float range = GetSceneLightRange(lightType);
-//    float emission = range / 15.0;
-//
-//    //float emission = GetSceneLightEmission(lightType);
-//    emission = _pow3(emission);
-//    return emission * Lighting_Brightness;
+#include "/lib/buffers/scene.glsl"
 
-    return 0.0;
-}
+#include "/lib/sampling/ign.glsl"
+#include "/lib/utility/lightmap.glsl"
+
+#ifdef WORLD_SKY_ENABLED
+    #include "/lib/world/sky.glsl"
+
+    #ifdef WORLD_WETNESS_ENABLED
+//        #include "/lib/world/wetness.glsl"
+//        #include "/lib/world/wetness_ripples.glsl"
+    #endif
+#endif
+
+#ifndef DEFERRED_BUFFER_ENABLED
+    #include "/lib/sampling/erp.glsl"
+    #include "/lib/sampling/noise.glsl"
+
+    #ifdef WORLD_SKY_ENABLED
+        #include "/lib/clouds/cloud_common.glsl"
+        #include "/lib/world/lightning.glsl"
+    #endif
+
+    #include "/lib/lighting/fresnel.glsl"
+    #include "/lib/lighting/sampling.glsl"
+    #include "/lib/lighting/scatter_transmit.glsl"
+
+    #ifdef IS_LPV_ENABLED
+        #include "/lib/buffers/volume.glsl"
+
+        #include "/lib/voxel/lpv/lpv.glsl"
+        #include "/lib/voxel/lpv/lpv_render.glsl"
+    #endif
+
+    #if MATERIAL_REFLECTIONS != REFLECT_NONE
+        #if defined(WORLD_SKY_ENABLED) && defined(MATERIAL_REFLECT_CLOUDS)
+            #if SKY_CLOUD_TYPE == CLOUDS_CUSTOM
+                #include "/lib/clouds/cloud_custom.glsl"
+            #elif SKY_CLOUD_TYPE == CLOUDS_VANILLA
+                #include "/lib/clouds/cloud_vanilla.glsl"
+            #endif
+        #endif
+
+        #include "/lib/lighting/reflections.glsl"
+    #endif
+
+    #if defined WORLD_SKY_ENABLED && LIGHTING_MODE != LIGHTING_MODE_NONE
+        #include "/lib/sky/irradiance.glsl"
+        #include "/lib/sky/sky_lighting.glsl"
+    #endif
+
+    #if LIGHTING_MODE == LIGHTING_MODE_TRACED
+        #include "/lib/lighting/traced.glsl"
+    #elif LIGHTING_MODE == LIGHTING_MODE_FLOODFILL
+        #include "/lib/lighting/floodfill.glsl"
+    #else
+        #include "/lib/lighting/vanilla.glsl"
+    #endif
+
+    //#include "/lib/lighting/basic_hand.glsl"
+#endif
+
 
 void voxy_emitFragment(VoxyFragmentParameters parameters) {
-    // Get base properties
+    vec4 color = parameters.sampledColour;
+    color.rgb *= parameters.tinting.rgb;
 
-    vec3 base_color = parameters.sampledColour.rgb * parameters.tinting.rgb;
-    base_color = RGBToLinear(base_color);
-
-    // from Cortex
-    vec3 normal = vec3(
+    vec3 localGeoNormal = vec3(
         uint((parameters.face >> 1) == 2),
         uint((parameters.face >> 1) == 0),
         uint((parameters.face >> 1) == 1)
     ) * (float(int(parameters.face) & 1) * 2.0 - 1.0);
 
-    uint material_mask = max(parameters.customId - 10000u, 0u);
+    vec3 albedo = RGBToLinear(color.rgb);
+    vec2 lmcoord = LightMapNorm(parameters.lightMap);
+    const float parallaxShadow = 1.0;
 
-    float roughness = GetBlockRoughness(parameters.customId);
-    float emission = computeBlockEmission(parameters.customId);
-    float metal_f0 = GetBlockMetalF0(parameters.customId);
-    float sss = GetBlockSSS(parameters.customId);
+    const float roughness = 0.86;//GetBlockRoughness(parameters.customId);
+    const float emission = 0.0;//computeBlockEmission(parameters.customId);
+    const float metal_f0 = 0.04;//GetBlockMetalF0(parameters.customId);
+    const float sss = 0.0;//GetBlockSSS(parameters.customId);
     const float occlusion = 1.0;
     const float porosity = 0.0;
 
-    const float parallaxShadow = 0.0;
-    vec2 lmFinal = LightMapNorm(parameters.lightMap);
-
-    // Encode gbuffer data
-//    gbuffer_data_0.x  = pack_unorm_2x8(base_color.rg);
-//    gbuffer_data_0.y  = pack_unorm_2x8(base_color.b, clamp01(float(material_mask) * rcp(255.0)));
-//    gbuffer_data_0.z  = pack_unorm_2x8(encode_unit_vector(normal));
-//    gbuffer_data_0.w  = pack_unorm_2x8(parameters.lightMap);
-
     #if defined DEFERRED_BUFFER_ENABLED || defined EFFECT_SSAO_ENABLED
-        outDeferredTexNormal = normal * 0.5 + 0.5;
+        outDeferredTexNormal = localGeoNormal * 0.5 + 0.5;
     #endif
 
     #ifdef DEFERRED_BUFFER_ENABLED
-        #if defined WORLD_SKY_ENABLED && defined WORLD_WETNESS_ENABLED
-            ApplySkyWetness(roughness, porosity, skyWetness, puddleF);
-        #endif
+//        #if defined WORLD_SKY_ENABLED && defined WORLD_WETNESS_ENABLED
+//            ApplySkyWetness(roughness, porosity, skyWetness, puddleF);
+//        #endif
 
         float dither = (InterleavedGradientNoise() - 0.5) / 255.0;
 
@@ -95,21 +135,135 @@ void voxy_emitFragment(VoxyFragmentParameters parameters) {
         #endif
 
         color.rgb = LinearToRGB(albedo);
-
-        // color.r = (vIn.blockId % 4) / 4.0;
-        // color.g = (vIn.blockId % 8) / 8.0;
-        // color.b = (vIn.blockId % 16) / 16.0;
-
-//        if (!all(lessThan(abs(texNormal), EPSILON3)))
-//            texNormal = texNormal * 0.5 + 0.5;
-
         outDeferredColor = color + dither;
 
-        outDeferredData.r = packUnorm4x8(vec4(normal * 0.5 + 0.5, sss + dither));
-        outDeferredData.g = packUnorm4x8(vec4(lmFinal, occlusion, emission) + dither);
+        outDeferredData.r = packUnorm4x8(vec4(localGeoNormal * 0.5 + 0.5, sss + dither));
+        outDeferredData.g = packUnorm4x8(vec4(lmcoord, occlusion, emission) + dither);
         outDeferredData.b = packUnorm4x8(vec4(0.0, parallaxShadow, 0.0, 0.0) + dither);
         outDeferredData.a = packUnorm4x8(vec4(roughness, metal_f0, porosity, 1.0) + dither);
     #else
-        // TODO
+        float roughL = _pow2(roughness);
+
+        vec3 shadowColor = vec3(1.0);
+        #ifdef RENDER_SHADOWS_ENABLED
+            #ifndef IRIS_FEATURE_SSBO
+                vec3 localSkyLightDirection = normalize((gbufferModelViewInverse * vec4(shadowLightPosition, 1.0)).xyz);
+            #endif
+
+            float skyGeoNoL = dot(localNormal, localSkyLightDirection);
+
+            if (skyGeoNoL < EPSILON && sss < EPSILON) {
+                shadowColor = vec3(0.0);
+            }
+            else {
+                #if SHADOW_TYPE == SHADOW_TYPE_CASCADED
+                    float shadowFade = 0.0;
+                    float lmShadow = 1.0;
+                #else
+                    float shadowFade = float(vIn.shadowPos != clamp(vIn.shadowPos, -1.0, 1.0));
+
+                    float lmShadow = pow(lmcoord.y, 9);
+                    if (vIn.shadowPos == clamp(vIn.shadowPos, -0.85, 0.85)) lmShadow = 1.0;
+                #endif
+
+                #ifdef SHADOW_COLORED
+                    // shadowColor = GetFinalShadowColor(localSkyLightDirection, shadowFade, sss);
+                    //if (vIn.shadowPos == clamp(vIn.shadowPos, -1.0, 1.0))
+                    if (shadowFade < 1.0)
+                        shadowColor = GetFinalShadowColor(localSkyLightDirection, shadowFade, sss);
+
+                    //shadowColor = mix(shadowColor, vec3(lmShadow), shadowFade);
+                    shadowColor = min(shadowColor, vec3(lmShadow));
+                #else
+                    float shadowF = 1.0;
+                    // if (vIn.shadowPos == clamp(vIn.shadowPos, -1.0, 1.0))
+                    if (shadowFade < 1.0)
+                        shadowF = GetFinalShadowFactor(localSkyLightDirection, shadowFade, sss);
+
+                    // float shadowF = GetFinalShadowFactor(localSkyLightDirection, shadowFade, sss);
+                    //shadowF = mix(shadowF, lmShadow, shadowFade);
+                    shadowF = min(shadowF, lmShadow);
+                    shadowColor = vec3(shadowF);
+                #endif
+            }
+        #else
+            // shadowColor = vec3(pow(lmcoord.y, 9));
+        #endif
+
+        // #if defined WORLD_SKY_ENABLED && defined RENDER_CLOUD_SHADOWS_ENABLED && SKY_CLOUD_TYPE > CLOUDS_VANILLA
+        //     float cloudShadow = TraceCloudShadow(cameraPosition + vIn.localPos, localSkyLightDirection, CLOUD_GROUND_SHADOW_STEPS);
+        //     deferredShadow.rgb *= 1.0 - (1.0 - cloudShadow) * (1.0 - Shadow_CloudBrightnessF);
+        // #endif
+
+//        #if defined WORLD_SKY_ENABLED && defined WORLD_WETNESS_ENABLED
+//            ApplySkyWetness(albedo, roughness, porosity, skyWetness, puddleF);
+//        #endif
+
+        vec3 diffuseFinal = vec3(0.0);
+        vec3 specularFinal = vec3(0.0);
+
+        vec3 ndcPos = gl_FragCoord.xyz;
+        ndcPos.xy /= viewSize;
+        ndcPos = ndcPos * 2.0 - 1.0;
+
+        vec3 viewPos = unproject(gbufferProjectionInverse, ndcPos);
+        vec3 localPos = mul3(gbufferModelViewInverse, viewPos);
+
+        #if LIGHTING_MODE == LIGHTING_MODE_FLOODFILL
+            GetFloodfillLighting(diffuseFinal, specularFinal, localPos, localNormal, texNormal, lmcoord, shadowColor, albedo, metal_f0, roughL, occlusion, sss, false);
+
+            diffuseFinal += emission * MaterialEmissionF;
+        #elif LIGHTING_MODE < LIGHTING_MODE_FLOODFILL
+            GetVanillaLighting(diffuseFinal, lmcoord, shadowColor, occlusion);
+        #endif
+
+        #if defined WORLD_SKY_ENABLED && LIGHTING_MODE != LIGHTING_MODE_NONE
+            const bool tir = false;
+            const bool isUnderWater = false;
+            GetSkyLightingFinal(diffuseFinal, specularFinal, shadowColor, localPos, localNormal, texNormal, albedo, lmcoord, roughL, metal_f0, occlusion, sss, isUnderWater, tir);
+        #else
+            diffuseFinal += WorldAmbientF;
+        #endif
+
+//        #if LIGHTING_MODE_HAND != HAND_LIGHT_NONE
+//            SampleHandLight(diffuseFinal, specularFinal, localPos, localNormal, texNormal, albedo, roughL, metal_f0, occlusion, sss);
+//        #endif
+
+        #if MATERIAL_SPECULAR != SPECULAR_NONE && LIGHTING_MODE != LIGHTING_MODE_NONE
+            ApplyMetalDarkening(diffuseFinal, specularFinal, albedo, metal_f0, roughL);
+        #endif
+
+        #if LIGHTING_MODE == LIGHTING_MODE_FLOODFILL
+            color.rgb = GetFinalLighting(albedo, diffuseFinal, specularFinal, occlusion);
+        #elif LIGHTING_MODE < LIGHTING_MODE_FLOODFILL
+            color.rgb = GetFinalLighting(albedo, diffuseFinal, specularFinal, metal_f0, roughL, emission, occlusion);
+        #endif
+
+        #ifdef SKY_BORDER_FOG_ENABLED
+            ApplyFog(color, localPos, localViewDir);
+            color.a = 1.0;
+        #endif
+
+        #if defined WORLD_SKY_ENABLED && LIGHTING_VOLUMETRIC != VOL_TYPE_NONE //&& SKY_CLOUD_TYPE <= CLOUDS_VANILLA
+            #ifdef WORLD_WATER_ENABLED
+                if (isEyeInWater == 0) {
+            #endif
+
+            float maxDist = min(viewDist, far);
+
+            vec3 vlLight = (phaseAir + AirAmbientF) * WorldSkyLightColor;
+            vec4 scatterTransmit = ApplyScatteringTransmission(maxDist, vlLight, AirScatterColor, AirExtinctColor);
+            color.rgb = color.rgb * scatterTransmit.a + scatterTransmit.rgb;
+
+            #ifdef WORLD_WATER_ENABLED
+                }
+            #endif
+        #endif
+
+        outFinal = color;
+    #endif
+
+    #ifdef EFFECT_TAA_ENABLED
+        outVelocity = vec4(0.0);
     #endif
 }
--- shaders/world0/voxy_opaque.glsl
+++ shaders/world0/voxy_opaque.glsl
@@ -1,5 +1,3 @@
-#version 430 compatibility
-
 #define RENDER_OPAQUE
 
 #include "overworld.glsl"
