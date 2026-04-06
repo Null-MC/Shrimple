156bdf26790d08f0614b55793215f9b4d485475c
NULL511 <null511@outlook.com>
2026-04-05 22:38:16 -0400

    water tweaks

--- shaders/lib/water-waves.glsl
+++ shaders/lib/water-waves.glsl
@@ -1,35 +1,27 @@
 const float PI_2 = PI * 2.0;
 
 
-float wave_gerstner(vec2 position, vec2 direction, float freq, float amp, float speed, float steepness) {
-    float phase = dot(position, direction) * freq * PI_2 + frameTimeCounter * speed;
-    return amp * pow(sin(mod(phase, PI_2)) * 0.5 + 0.5, steepness);
-}
-
-float wave_gerstner2(vec2 position, vec2 direction, float freq, float speed) {
+float wave_gerstner(vec2 position, float angle, float freq, float speed, float steepness) {
+    vec2 direction = normalize(vec2(cos(angle), sin(angle)));
     float x = mod(dot(position, direction) * freq + frameTimeCounter * speed, PI_2);
-//    return -exp(sin(x) - 1.0) * cos(x);
-    return exp(sin(x) - 1.0);
+    return pow(exp(sin(x) - 1.0), steepness);
 }
 
 float wave_fbm(const in vec2 position, const in int count) {
     float height = 0.0;
-    float freq = 0.4;    // Base integer frequency for tiling
-    float amp = 0.3;     // Base amplitude
-    float speed = 1.5;   // Base speed
-//    float steepness = 1.5; // Increases the sharpness of the crests
+    float freq = 0.7;    // Base integer frequency for tiling
+    float amp = 0.12;     // Base amplitude
+    float speed = 1.8;   // Base speed
+    float steepness = 1.0;
 
     for (int i = 0; i < count; i++) {
-        float angle = float(i) * 1.124 + 0.13;
-        vec2 dir = normalize(vec2(cos(angle), sin(angle)));
+        float angle = float(i) * 2.76 + 0.13;
+        height += wave_gerstner(position, angle, freq, speed, steepness) * amp;
 
-//        height += wave_gerstner(position, dir, freq, amp, speed, steepness);
-        height += wave_gerstner2(position, dir, freq, speed) * amp;
-
-        freq *= 1.42;
+        freq *= 1.32;
         amp *= 0.52;
-        speed *= 1.2;
-//        steepness *= 1.2;
+        speed *= 1.12;
+        steepness *= 1.05;
     }
 
     return saturate(height);
